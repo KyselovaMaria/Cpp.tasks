54de6053e3361b5b982edefe656b218d9dc162c9
KyselovaMaria <88087036+KyselovaMaria@users.noreply.github.com>
2021-09-30 16:13:52 +0300

    HW2 2.14 Kyselova Maria

---
+++ HW2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void findRoots(int a, int b, int c)
+{
+	
+	if (a == 0) {
+		cout << "Error";
+		return;
+	}
+
+	int d = b * b - 4 * a * c;
+	double sqrt_val = sqrt(abs(d));
+
+	if (d > 0) {
+		cout << "Roots are real and different \n";
+		cout << (double)(-b + sqrt_val) / (2 * a) << "\n"
+			<< (double)(-b - sqrt_val) / (2 * a);
+	}
+	else 
+	{
+		cout << "Roots are real and same \n";
+		cout << -(double)b / (2 * a);
+	}
+	
+}
+
+int main()
+{
+	//int a = 3, b = 100, c = 2;
+	double a, b, c;
+    cin >> a >> b >> c;
+	findRoots(a, b, c);
+	return 0;
+}
+
