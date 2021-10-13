69434f377ca15c709e98accbc59b7cdb6189bd45
KyselovaMaria <88087036+KyselovaMaria@users.noreply.github.com>
2021-10-13 17:34:25 +0300

    Update 4.8.cpp

--- 4.8.cpp
+++ 4.8.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
+
 int main()
-{
-    int m; 
+{ 
+    int m, k=0; 
     cout << "m=" ;
     cin >> m;
-    int k = (int)m / 4;
+    while (pow(4, k) <= m) { 
+        k++; 
+    } 
+    k--; 
     cout << "k = "<< k << endl;
     return 0;
-}
+} 
